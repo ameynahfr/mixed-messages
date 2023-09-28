4a366ee4a8b6d4aad378631db1296c8f7db08317
Aminah Shahzad <135119713+ameynahfr@users.noreply.github.com>
2023-09-28 14:28:16 +0500

    UI ADDED

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
-const affirmationChallenge = {
-  confidence: [
-    "I am confident and capable.",
-    "I believe in my abilities.",
-    "I embrace challenges with courage.",
-    "I trust in my decisions and embrace uncertainty.",
-    "I am fearless in the face of challenges and setbacks."
-  ],
-  gratitude: [
-    "I am grateful for the abundance in my life.",
-    "I appreciate the small joys that surround me.",
-    "I acknowledge the kindness of others.",
-    "I am thankful for the love and support of my friends and family.",
-    "I appreciate the beauty of nature that surrounds me."
-  ],
-  positivity: [
-    "I radiate positivity and attract positive energy.",
-    "I choose to focus on the bright side of life.",
-    "I am a magnet for happiness.",
-    "I approach each day with a positive mindset and an open heart.",
-    "I am a source of inspiration and positivity for those around me."
-  ]
-};
+// Get references to the button and the <p> tag
+const runButton = document.getElementById('runButton');
+const outputP = document.getElementById('output');
 
-// Function to select a random index from an array
-function getRandomIndex(arr) {
-  return Math.floor(Math.random() * arr.length);
+// Define your script logic
+function runScript() {
+    // Your script logic here
+    const affirmationChallenge = {
+        confidence: [
+            "I am confident and capable.",
+            "I believe in my abilities.",
+            "I embrace challenges with courage.",
+            "I trust in my decisions and embrace uncertainty.",
+            "I am fearless in the face of challenges and setbacks."
+        ],
+        gratitude: [
+            "I am grateful for the abundance in my life.",
+            "I appreciate the small joys that surround me.",
+            "I acknowledge the kindness of others.",
+            "I am thankful for the love and support of my friends and family.",
+            "I appreciate the beauty of nature that surrounds me."
+        ],
+        positivity: [
+            "I radiate positivity and attract positive energy.",
+            "I choose to focus on the bright side of life.",
+            "I am a magnet for happiness.",
+            "I approach each day with a positive mindset and an open heart.",
+            "I am a source of inspiration and positivity for those around me."
+        ]
+    };
+
+    // Function to select a random index from an array
+    function getRandomIndex(arr) {
+        return Math.floor(Math.random() * arr.length);
+    }
+
+    let outputHTML = ''; // Initialize an empty string to accumulate output
+
+    for (const key in affirmationChallenge) {
+        const category = affirmationChallenge[key];
+        const randomIndex = getRandomIndex(category);
+        outputHTML += `Selected Theme: ${key}<br>Affirmation: ${category[randomIndex]}<br>---<br>`;
+    }
+
+    // Update the innerHTML of the <p> tag with the accumulated HTML
+    outputP.innerHTML = outputHTML;
 }
 
-console.log(`Today's Affirmations`)
-
-for (const key in affirmationChallenge) {
-  const category = affirmationChallenge[key];
-  const randomIndex = getRandomIndex(category);
-  console.log(`Selected Theme: ${key}`);
-  console.log(`Affirmation: ${category[randomIndex]}`);
-  console.log('---');
-}
+// Add a click event listener to the button
+runButton.addEventListener('click', runScript);
